d61d8390b8f479cfaab7537a33be5b54a1ef0955
soph-astronauta <161590070+soph-astronauta@users.noreply.github.com>
2025-08-04 09:03:31 -0300

    Add files via upload

---
+++ biblioteca.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Ago-2025 às 14:03
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bibioteca_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `biblioteca`
+--
+-- Erro ao ler a estrutura para a tabela bibioteca_bd.biblioteca: #1146 - Tabela &#039;bibioteca_bd.biblioteca&#039; não existe
+-- Erro ao ler dados para tabela bibioteca_bd.biblioteca: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `bibioteca_bd`.`biblioteca`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
